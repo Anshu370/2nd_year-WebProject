0179eeacaaa1f4b6338c542ee99ae3be64561c42
Anshu Gupta <77850344+Anshu370@users.noreply.github.com>
2023-12-24 04:09:51 +0530

    Add files via upload

---
+++ js/catalouge.js
@@ -0,0 +1,35 @@
+var arr = [];
+
+function addTocart(name, quantity, amount) {
+    let desc = {'name':name, 'quantity':quantity, 'amount':amount}
+    for(let i = 0;i<arr.length;i++){
+        if(arr[i]['name'] == desc['name']){
+            arr[i]['quantity'] += 1
+            return;
+        }
+    }
+    arr.push(desc)
+}
+
+console.log(arr)
+
+function clearCart(){
+    console.log(arr)
+}
+
+// function cart() {
+//     var st = ""; var pp = document.getElementById('desc');
+//     var ptot = document.getElementById('tot');
+//     for (let i = 0; i < arr.length; i++)
+
+//         st = st + arr[i].price.toString() + "------------" + arr[i].qty.toString() + "------------" + arr[i].amt.toString() + "\n"
+
+
+//     pp.innerText = st;
+//     var t = 0;
+
+//     for (let i = 0; i < arr.length; i++)
+//         t = t + parseInt(arr[i].amt)//.toString();
+
+//     ptot.innerText = "Total " + t;
+// }
---
+++ js/login.js
@@ -0,0 +1,9 @@
+function clearAllField() {
+    document.getElementById('username').value = "";
+    document.getElementById('password').value = "";
+}
+
+function submitData() {
+    var usernameData = document.getElementById('username').value;
+    var passwordData = document.getElementById('password').value;
+}
